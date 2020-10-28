a9ccca9fd6356dd83fb101a70a9107cf5ea1d9a9
Zavier Divelbiss <semiviral@gmail.com>
2020-10-27 22:03:17 -0500

    add project files

---
+++ Generic-Random/GenericRandom.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Generic_Random
+{
+    public class GenericRandom
+    {
+        //
+        // Private Constants
+        //
+        private const int _MBIG = int.MaxValue;
+        private const int _MSEED = 161803398;
+        private const int _MZ = 0;
+
+        //
+        // Member Variables
+        //
+        private int _NextInt;
+        private int _NextIntP;
+        private int[] _SeedArray = new int[56];
+
+        public GenericRandom(int seed)
+        {
+            int ii = 0;
+            int mj, mk = 1;
+
+            //Initialize our Seed array.
+            int subtraction = seed == int.MinValue ? int.MaxValue : Math.Abs(seed);
+            _SeedArray[55] = mj = _MSEED - subtraction;
+
+            for (int i = 1; i < 55; i++)
+            {
+                //Apparently the range [1..55] is special (Knuth) and so we're wasting the 0'th position.
+                if ((ii += 21) >= 55) ii -= 55;
+                _SeedArray[ii] = mk;
+                mk = mj - mk;
+                if (mk < 0) mk += _MBIG;
+                mj = _SeedArray[ii];
+            }
+
+            for (int k = 1; k < 5; k++)
+            {
+                for (int i = 1; i < 56; i++)
+                {
+                    int n = i + 30;
+                    if (n >= 55) n -= 55;
+                    _SeedArray[i] -= _SeedArray[1 + n];
+                    if (_SeedArray[i] < 0) _SeedArray[i] += _MBIG;
+                }
+            }
+
+            _NextInt = 0;
+            _NextIntP = 21;
+        }
+
+        public T Next<T>() where T : unmanaged
+        {
+            if (typeof(T) == typeof(float))
+            {
+                float sample = InternalSample() * (1f / _MBIG);
+                return Unsafe.As<float, T>(ref sample);
+            }
+            else if (typeof(T) == typeof(double))
+            {
+                double sample = InternalSample() * (1d / _MBIG);
+                return Unsafe.As<double, T>(ref sample);
+            }
+            else if (typeof(T) == typeof(long))
+            {
+                // cast low to uint to avoid losing sign
+                uint sampleLow = (uint)InternalSample();
+                int sampleHigh = InternalSample();
+
+                long sample = ((long)sampleHigh << 32) | sampleLow;
+                return Unsafe.As<long, T>(ref sample);
+            }
+            else if (typeof(T) == typeof(ulong))
+            {
+                // cast low to uint to avoid losing sign
+                uint sampleLow = (uint)InternalSample();
+                int sampleHigh = InternalSample();
+
+                ulong sample = ((ulong)sampleHigh << 32) | sampleLow;
+                return Unsafe.As<ulong, T>(ref sample);
+            }
+            else if ((typeof(T) == typeof(int))
+                     || (typeof(T) == typeof(uint))
+                     || (typeof(T) == typeof(short))
+                     || (typeof(T) == typeof(ushort))
+                     || (typeof(T) == typeof(byte))
+                     || (typeof(T) == typeof(sbyte)))
+            {
+                int sample = InternalSample();
+                return Unsafe.As<int, T>(ref sample);
+            }
+            else throw new NotSupportedException("T is of unsupported type.");
+        }
+
+        public void Next<T>(Span<T> fill) where T : unmanaged
+        {
+            if (fill.Length == 0) return;
+
+            for (int index = 0; index < fill.Length; index++) fill[index] = Next<T>();
+        }
+
+        private int InternalSample()
+        {
+            int retVal;
+            int locINext = _NextInt;
+            int locINextp = _NextIntP;
+
+            if (++locINext >= 56) locINext = 1;
+            if (++locINextp >= 56) locINextp = 1;
+
+            retVal = _SeedArray[locINext] - _SeedArray[locINextp];
+
+            if (retVal == _MBIG) retVal--;
+            if (retVal < 0) retVal += _MBIG;
+
+            _SeedArray[locINext] = retVal;
+
+            _NextInt = locINext;
+            _NextIntP = locINextp;
+
+            return retVal;
+        }
+    }
+}
---
+++ Generic-Random/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Generic_Random
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Summary _ = BenchmarkRunner.Run<RandomBenchmark>();
+            Console.ReadLine();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter]
+    public class RandomBenchmark
+    {
+        private const int _RANDOM_SEED = 783247234;
+        private const int _ITERATIONS = 100000;
+        private byte[] _Bytes;
+        private double[] _Doubles;
+
+        private GenericRandom _GenericRandom;
+
+        private int[] _Ints;
+        private long[] _Longs;
+        private Random _Random;
+        private short[] _Shorts;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _GenericRandom = new GenericRandom(_RANDOM_SEED);
+            _Random = new Random(_RANDOM_SEED);
+
+            _Ints = new int[_ITERATIONS];
+            _Shorts = new short[_ITERATIONS];
+            _Bytes = new byte[_ITERATIONS];
+            _Doubles = new double[_ITERATIONS];
+            _Longs = new long[_ITERATIONS];
+        }
+
+        [Benchmark]
+        public int[] GenericRandomNextInt()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Ints[index] = _GenericRandom.Next<int>();
+
+            return _Ints;
+        }
+
+        [Benchmark]
+        public int[] RandomNextInt()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Ints[index] = _Random.Next();
+
+            return _Ints;
+        }
+
+        [Benchmark]
+        public short[] GenericRandomNextShort()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Shorts[index] = _GenericRandom.Next<short>();
+
+            return _Shorts;
+        }
+
+        [Benchmark]
+        public short[] RandomNextShort()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Shorts[index] = (short)_Random.Next();
+
+            return _Shorts;
+        }
+
+        [Benchmark]
+        public byte[] GenericRandomNextByte()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Bytes[index] = _GenericRandom.Next<byte>();
+
+            return _Bytes;
+        }
+
+        [Benchmark]
+        public byte[] RandomNextByte()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Bytes[index] = (byte)_Random.Next();
+
+            return _Bytes;
+        }
+
+        [Benchmark]
+        public double[] GenericRandomNextDouble()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Doubles[index] = _GenericRandom.Next<double>();
+
+            return _Doubles;
+        }
+
+        [Benchmark]
+        public double[] RandomNextDouble()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Doubles[index] = _Random.NextDouble();
+
+            return _Doubles;
+        }
+
+        [Benchmark]
+        public long[] GenericRandomNextLong()
+        {
+            for (int index = 0; index < _ITERATIONS; index++) _Longs[index] = _GenericRandom.Next<long>();
+
+            return _Longs;
+        }
+
+        [Benchmark]
+        public long[] RandomNextLong()
+        {
+            for (int index = 0; index < _ITERATIONS; index++)
+            {
+                int low = _Random.Next();
+                int high = _Random.Next();
+                _Longs[index] = ((long)high << 32) | (uint)low;
+            }
+
+            return _Longs;
+        }
+    }
+}
